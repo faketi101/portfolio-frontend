79a9ac6a07b6630b2d5505553295973f746e5dad
faketi101 <tamim85000@gmail.com>
2021-06-06 22:13:36 +0600

    school mode added

--- client/src/pages/components/Freeelancer.jsx
+++ client/src/pages/components/Freeelancer.jsx
@@ -1,7 +1,7 @@
 const Freelance = () => {
   return (
     <section id="freelancer" className="freelancer">
-      <h1>I am Available for Freelancer</h1>
+      <h1>I am Available for Development</h1>
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam,
         earum.
